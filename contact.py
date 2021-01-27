a6d1cfaad19d4d25ed3bd8560096a0ec2e91faf7
pacwman <kaass@o2.pl>
2021-01-27 17:26:09 +0100

    contact

---
+++ contact.py
@@ -0,0 +1,16 @@
+class Contact:
+    def __init__(self, firstname, lastname, company, address, telephone, mobile, tel_work, email, email_1, www,
+                 birth_day, birth_month, birth_year):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.telephone = telephone
+        self.mobile = mobile
+        self.tel_work = tel_work
+        self.email = email
+        self.email_1 = email_1
+        self.www = www
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
