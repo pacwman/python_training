ee1ea1ffe151901bce07199beb9df9636743f14a
pacwman <kaass@o2.pl>
2021-02-02 10:56:42 +0100

    class Session_helper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,15 +12,15 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Group1", header="New group", footer="no comments"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
